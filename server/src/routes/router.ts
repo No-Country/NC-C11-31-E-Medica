b615d668d9b5b50d9c255d20ee8eea77b543f05f
ChrFont <roblesfontc@gmail.com>
2023-05-15 12:03:28 -0400

    add/health-check-route

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import specialtyRouter from './specialtyRouter'
-
+import healthCheckRouter from './healthCheckRouter'
 const router = express.Router()
+
+router.use('/health-check', healthCheckRouter)
 
 router.use('/specialty', specialtyRouter)
 
