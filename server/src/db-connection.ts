93bf90599031c62b27de9544679a5a5559509dd8
Juan Leiton <jgleitonl@gmail.com>
2023-05-14 22:06:14 -0500

    MongoDB set up

---
+++ server/src/db-connection.ts
@@ -0,0 +1,17 @@
+import * as dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
+mongoose.set('strictQuery', true)
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env
+const validEnv = typeof DB_USER === 'string' && typeof DB_PASSWORD === 'string' && typeof DB_HOST === 'string' && typeof DB_NAME === 'string'
+const connection = validEnv ? `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority` : 'invalid connection'
+
+mongoose.connect(connection)
+  .then(() => {
+    console.log('E-Medica is connected to', DB_NAME)
+  })
+  .catch(error => {
+    console.error('Failed connection', error.message)
+  })
