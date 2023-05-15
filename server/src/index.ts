eee10613970239e05c089a732231d4fd2a00412d
Juan Leiton <jgleitonl@gmail.com>
2023-05-14 22:04:34 -0500

    Entry point template added

---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import templateRouter from './routes/templateRoutes'
+
+const app = express()
+
+// env variables
+dotenv.config()
+const { SERVER_LOCAL_PORT } = process.env
+
+// settings
+app.set('port', process.env.PORT ?? SERVER_LOCAL_PORT)
+
+// middlewares
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors())
+
+// routes
+app.use('/', templateRouter)
+
+app.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'))
+})
