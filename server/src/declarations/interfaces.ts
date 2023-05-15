938b5cd6c881059704b842e835a31738fa9d3d4a
Juan Leiton <jgleitonl@gmail.com>
2023-05-14 22:09:56 -0500

    declaration templates added to boilerplate

---
+++ server/src/declarations/interfaces.ts
@@ -0,0 +1,13 @@
+import { Date, Types, Document } from 'mongoose'
+import { EnumExample } from './enums'
+
+export interface IExample extends Document {
+  propExample1: string
+  propExample2: Types.ObjectId
+  propExample3: number
+  propExample4: string[]
+  propExample5: Types.ObjectId[]
+  propExample6: boolean
+  propExample7: Date
+  propExample8: EnumExample
+}
