274615abcc557bcf5325ea54814fffb69a55d515
ChrFont <roblesfontc@gmail.com>
2023-05-16 08:48:44 -0400

    add/interfaces-patient-specialist-appointment

--- server/src/declarations/enums.ts
+++ server/src/declarations/enums.ts
@@ -3,3 +3,17 @@ export enum EnumExample {
   value2 = 'value2',
   value3 = 'value3',
 }
+
+export enum EnumGender {
+  value1 = 'male',
+  value2 = 'female',
+  value3 = 'other',
+}
+
+export enum EnumStatus {
+  value1 = 'schedule',
+  value2 = 'canceled',
+  value3 = 'missed',
+  value4 = 'attended',
+  value5 = 'rated',
+}
