9abadac29ef546bfa20c985d0f31067192ca2fc8
demattei-teo <teitodematteio@gmail.com>
2023-05-17 18:16:26 -0300

    refactor: interface home

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,25 +1,16 @@
-import {Hero} from './components/Hero/Hero'
+import { Hero } from './components/Hero/Hero'
 import { CardSpecialties } from './components/CardSpecialties/CardSpecialties'
 import TurnSteps from './components/turnSteps/TurnSteps'
-
+import WrapperGrid from './components/specielties/Wrapper-grid'
 const Page = () => {
-  
-  const specialties = ['Cardiología','Odontología', 'Traumatología', 'Obstetricia', 'Oftalmología', 'Dermatología', 'Pediatría', 'Cirugía']
-  
   return (
-    <div>
+    <>
       <Hero />
-      <div className='wrapperTexts'>
-        <h2>Nuestros servicios</h2>
-        <p>Contamos con +200 servicios para vos y tu familia</p>
-      </div>
-      <div className='wrapperGrid'>
-        {specialties.map(specielty => <CardSpecialties specielty={specielty}/>)}
-      </div>
+
+      <WrapperGrid />
       <TurnSteps />
-    </div>
+    </>
   )
-  
 }
 
-export default page
+export default Page
