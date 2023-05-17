cdf76777432ff42e85af9f30f2b47609aa275760
demattei-teo <teitodematteio@gmail.com>
2023-05-16 21:29:01 -0300

    chore: change file structure

--- client/src/app/components/Navbar/Navbar.jsx
+++ client/src/app/components/Navbar/Navbar.jsx
@@ -1,28 +1,26 @@
-import Link from "next/link";
-import React from "react";
-import "./Navbar.css";
+import Link from 'next/link'
 export default function Navbar() {
   return (
-    <header className="navbar-container">
-      <input type="text" className="navbar-input" />
-      <ul className="navbar-ul-container">
-        <li className="navbar-li-container">
-          <div className="navbar-img" />
+    <header className='navbar-container'>
+      <input type='text' className='navbar-input' />
+      <ul className='navbar-ul-container'>
+        <li className='navbar-li-container'>
+          <div className='navbar-img' />
           <label>Label</label>
         </li>
-        <li className="navbar-li-container">
-          <div className="navbar-img" />
+        <li className='navbar-li-container'>
+          <div className='navbar-img' />
           <label>Label</label>
         </li>
-        <li className="navbar-li-container">
-          <div className="navbar-img" />
+        <li className='navbar-li-container'>
+          <div className='navbar-img' />
           <label>Label</label>
         </li>
-        <li className="navbar-li-container">
-          <div className="navbar-img" />
+        <li className='navbar-li-container'>
+          <div className='navbar-img' />
           <label>Label</label>
         </li>
       </ul>
     </header>
-  );
+  )
 }
