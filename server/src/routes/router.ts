cdd7cc3d06154e2fab13c434b317ce3a4f112da1
Juan Leiton <jgleitonl@gmail.com>
2023-05-17 15:26:34 -0500

    Specialist router used

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import specialtyRouter from './specialtyRouter'
 import healthCheckRouter from './healthCheckRouter'
+import specialistRouter from './specialistRouter'
 const router = express.Router()
 
 router.use('/health-check', healthCheckRouter)
 
 router.use('/specialty', specialtyRouter)
+
+router.use('/specialist', specialistRouter)
 
 export default router
