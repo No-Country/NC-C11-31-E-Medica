d5a7924b1684c998e63d1f7c04d6226c21f7c3e7
Jose Cardozo <cardoz.jg@gmail.com>
2023-05-17 20:11:23 -0400

    carousel desktop & mobile

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,28 +1,37 @@
-
 "use client";
-import {Hero} from './components/Hero/Hero'
-import { CardSpecialties } from './components/CardSpecialties/CardSpecialties'
-import TurnSteps from './components/turnSteps/TurnSteps'
+import { Hero } from "./components/Hero/Hero";
+import { CardSpecialties } from "./components/CardSpecialties/CardSpecialties";
+import TurnSteps from "./components/turnSteps/TurnSteps";
+import Gallery from "./components/Gallery/Gallery";
 
 const Page = () => {
-  
-  const specialties = ['Cardiología','Odontología', 'Traumatología', 'Obstetricia', 'Oftalmología', 'Dermatología', 'Pediatría', 'Cirugía']
-  
+  const specialties = [
+    "Cardiología",
+    "Odontología",
+    "Traumatología",
+    "Obstetricia",
+    "Oftalmología",
+    "Dermatología",
+    "Pediatría",
+    "Cirugía",
+  ];
+
   return (
     <div>
       <Hero />
-      <div className='wrapperTexts'>
+      <div className="wrapperTexts">
         <h2>Nuestros servicios</h2>
         <p>Contamos con +200 servicios para vos y tu familia</p>
       </div>
-      <div className='wrapperGrid'>
-        {specialties.map(specielty => <CardSpecialties specielty={specielty}/>)}
+      <div className="wrapperGrid">
+        {specialties.map((specielty) => (
+          <CardSpecialties specielty={specielty} />
+        ))}
       </div>
       <TurnSteps />
+      <Gallery />
     </div>
-  )
-  
-}
+  );
+};
 
-export default page
-
+export default Page;
