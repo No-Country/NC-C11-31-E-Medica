c6f3e762cff2d54667dc5ed11ed669966c9c6370
Trinidad Atrio <triniatrio@gmail.com>
2023-05-18 12:40:34 -0300

    fix[frontend]: correcting errors

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,9 @@
 "use client";
 import {Hero} from './components/Hero/Hero'
-
-import { CardSpecialties } from './components/CardSpecialties/CardSpecialties'
 import TurnSteps from './components/turnSteps/TurnSteps'
-import WrapperGrid from './components/specielties/Wrapper-grid'
+import WrapperGrid from './components/specialties/Wrapper-grid'
+import Gallery from './components/Gallery/Gallery';
+
 const Page = () => {
   return (
     <>
