206cb1b9e5b8c4031402a3126cfa98594e20ae98
ChrFont <roblesfontc@gmail.com>
2023-05-22 10:34:24 -0400

    add/specialty-basic-validations

--- server/src/routes/specialtyRouter.ts
+++ server/src/routes/specialtyRouter.ts
@@ -1,20 +1,7 @@
 import express, { Request, Response, RequestHandler } from 'express'
-import { createSpecialty, getAllSpecialties } from '../controllers/specialtyController'
-
+import { getAllSpecialties, getSpecialtyById, getSpecialtyByName } from '../controllers/specialtyController'
+import { Types } from 'mongoose'
 const specialtyRouter = express.Router()
-
-// Ruta para crear una especialidad
-specialtyRouter.post('/', (async (req: Request, res: Response) => {
-  try {
-    const { name, description, active } = req.body
-
-    const specialty = await createSpecialty(name, description, active)
-
-    res.status(201).send({ specialty })
-  } catch (error) {
-    res.status(500).send({ error: 'Error al crear la especialidad' })
-  }
-}) as RequestHandler)
 
 specialtyRouter.get('/', (async (req: Request, res: Response) => {
   try {
@@ -25,4 +12,46 @@ specialtyRouter.get('/', (async (req: Request, res: Response) => {
     res.status(500).json({ error: 'No se pudieron obtener las especialidades' })
   }
 }) as RequestHandler)
+
+specialtyRouter.get('/:id', (async (req: Request, res: Response) => {
+  try {
+    const specialtyId = req.params.id;
+
+    if (!Types.ObjectId.isValid(specialtyId)) {
+      return res.status(400).json({ message: 'Identificador de especialidad inválido' });
+    }
+
+    const specialty = await getSpecialtyById(specialtyId);
+
+    if (specialty) {
+      return res.status(200).json(specialty);
+    } else {
+      return res.status(404).json({ message: 'Especialidad no encontrada' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error al obtener la especialidad' });
+  }
+}) as RequestHandler)
+
+specialtyRouter.get('/name/:name', (async (req: Request, res: Response) => {
+  try {
+    //   /name/Medicina General
+    const specialtyName = req.params.name
+
+    if (!specialtyName) {
+      return res.status(400).json({ message: 'Nombre no ingresado' });
+    }
+
+    const specialty = await getSpecialtyByName(specialtyName);
+
+    if (specialty) {
+      return res.status(200).json(specialty);
+    } else {
+      return res.status(404).json({ message: 'Especialidad no encontrada' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error al obtener la especialidad' });
+  }
+}) as RequestHandler)
+
 export default specialtyRouter
