8f775c2ffb89394fe3f7c479fe61ba5fb69e5b26
Juan Leiton <jgleitonl@gmail.com>
2023-05-24 16:10:54 -0500

    Linter and format issues solved in validations

--- server/src/validations/index.ts
+++ server/src/validations/index.ts
@@ -1,4 +1,4 @@
-import { body, param } from 'express-validator';
+import { body, param } from 'express-validator'
 import { Types } from 'mongoose'
 export const patientValidation = [
   body('firstName').notEmpty().withMessage('El nombre es obligatorio'),
@@ -7,18 +7,18 @@ export const patientValidation = [
   body('gender').notEmpty().withMessage('El género es obligatorio'),
   body('dni').notEmpty().withMessage('El DNI es obligatorio'),
   body('email').notEmpty().withMessage('El email es obligatorio').isEmail().withMessage('El email no es válido')
-];
+]
 
 export const idValidation = [
   param('id')
     .notEmpty().withMessage('Id no ingresado')
     .custom((value) => {
       if (!Types.ObjectId.isValid(value)) {
-        throw new Error('Identificador de paciente inválido');
+        throw new Error('Id inválido')
       }
-      return true;
-    }),
-];
+      return true
+    })
+]
 
 export const appointmentValidation = [
   body('dateTime').notEmpty().withMessage('La fecha es obligatoria'),
@@ -26,31 +26,33 @@ export const appointmentValidation = [
   body('paymentId').notEmpty().withMessage('El paymentId es obligatorio'),
   body('patient').notEmpty().withMessage('El ID del paciente es obligatorio').custom((value) => {
     if (!Types.ObjectId.isValid(value)) {
-      throw new Error('ID de paciente inválido');
+      throw new Error('ID de paciente inválido')
     }
-    return true;
+    return true
   }),
   body('specialist').notEmpty().withMessage('El ID del profesional es obligatorio').custom((value) => {
     if (!Types.ObjectId.isValid(value)) {
-      throw new Error('ID de profesional inválido');
+      throw new Error('ID de profesional inválido')
     }
-    return true;
+    return true
   }),
   body('specialty').notEmpty().withMessage('El ID de la especialidad es obligatorio').custom((value) => {
     if (!Types.ObjectId.isValid(value)) {
-      throw new Error('ID de especialidad inválido');
+      throw new Error('ID de especialidad inválido')
     }
-    return true;
+    return true
   })
 ]
 
 export const specialistValidation = [
   body('firstName').notEmpty().withMessage('El nombre es obligatorio.'),
   body('lastName').notEmpty().withMessage('El apellido es obligatorio.'),
+  body('picture').notEmpty().withMessage('La foto es obligatoria.'),
+  body('bio').notEmpty().withMessage('La biografía es obligatoria'),
   body('dni').notEmpty().withMessage('El dni es obligatorio.'),
   body('rup').notEmpty().withMessage('El rup es obligatorio.'),
   body('email').notEmpty().withMessage('El email es obligatorio.'),
-  body('signatureLink').notEmpty().withMessage('El enlace de la imagen de la firma es obligatorio.'),
+  body('signatureLink').notEmpty().withMessage('La imagen de la firma es obligatoria.'),
   body('mercadoPago').notEmpty().withMessage('El enlace de mercadoPago es obligatorio.'),
   body('specialty').notEmpty().withMessage('La especialidad es obligatoria.').custom(value => {
     if (!Types.ObjectId.isValid(value)) {
