e831e4d000230d7748a78d5c36060844c76668a8
Juan Leiton <jgleitonl@gmail.com>
2023-05-25 13:01:47 -0500

    auth/:email route added to authRouter

---
+++ server/src/routes/authRouter.ts
@@ -0,0 +1,29 @@
+import { Router, Request, Response, RequestHandler } from 'express'
+import { getPatientByEmail } from '../controllers/patientController'
+import { getSpecialistByEmail } from '../controllers/specialistController'
+import { validationResult } from 'express-validator'
+
+const authRouter = Router()
+
+// Ruta para obtener el usuario (especialista o paciente) que se autentica exitosamente
+authRouter.get('/:email', (async (req: Request, res: Response) => {
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: 'Errores de validación', errors: errors.array() })
+    }
+    const { email } = req.params
+    const specialist = await getSpecialistByEmail(email)
+    const patient = await getPatientByEmail(email)
+    if (specialist === null && patient === null) {
+      return res.status(404).json({ error: 'Usuario no encontrado.' })
+    }
+    const user = specialist === null ? { userType: 'patient', patient } : { userType: 'specialist', specialist }
+    res.status(200).json(user)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'No se encontró un usuario con el email ingresado.' })
+  }
+}) as RequestHandler)
+
+export default authRouter
