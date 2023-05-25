37736b9aea3716d7ed8c1a7f164cddd1fce9119f
Trinidad Atrio <triniatrio@gmail.com>
2023-05-25 12:20:29 -0300

    fix[frontend]: add new loading component

---
+++ client/src/app/components/Loading/Loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Loading = () => {
+  return (
+  <div class="text-center">
+    <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+  )
+}
+
+export default Loading
