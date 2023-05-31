25ebd9c19af161c7c55df8656197cfb61c6c383f
ChrFont <roblesfontc@gmail.com>
2023-05-31 18:16:43 -0400

    hotfix/renderSpecialties

--- client/src/app/components/specialties/Wrapper-grid.jsx
+++ client/src/app/components/specialties/Wrapper-grid.jsx
@@ -5,7 +5,6 @@ import { useEffect, useState } from 'react'
 const WrapperGrid = () => {
 
   const [specialties, setSpecialties] = useState([])
-  console.log('state:', specialties)
 
   useEffect(() => {
     getSpecialty().then(specialties => setSpecialties(specialties))
@@ -17,9 +16,10 @@ const WrapperGrid = () => {
         <h2>Nuestros servicios</h2>
         <p>Contamos con +200 servicios para vos y tu familia</p>
       </div>
-      {specialties.slice(0, 12).map((specielty, index) => (
-        <CardSpecialties specielty={specielty.name} key={index} id={specielty.id} />
-      ))}
+      {specialties.length > 7 &&
+        specialties.slice(0, 12).map((specielty, index) => (
+          <CardSpecialties specielty={specielty.name} key={index} id={specielty.id} />
+        ))}
     </div>
   )
 }
