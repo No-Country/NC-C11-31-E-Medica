d21be3ffacd05e638bcc44065b32ccecf7fcaade
Trinidad Atrio <triniatrio@gmail.com>
2023-06-08 01:18:33 -0300

    Add login request

--- client/src/app/components/Navbar/Navbar.jsx
+++ client/src/app/components/Navbar/Navbar.jsx
@@ -1,23 +1,41 @@
 'use client'
+import GlobalContext from '@/app/context/global/Global-context'
 import Link from 'next/link'
+import { useContext } from 'react'
+import DropdownMenu from './DropdownMenu'
 
 const Navbar = ({ valueNavbar }) => {
+  const {userData, setUserData} = useContext(GlobalContext);
+
+  const logOut = () =>{
+    setUserData({});
+  }
+
   return (
     <nav className={`nav ${valueNavbar ? 'openNavbar' : 'closeNavbar'}`}>
+      {!userData.firstName ? (
       <ul className='nav-list'>
-        <li>
-          <Link href='/register' className='nav-link'>
-            Registro
-          </Link>
-        </li>
-        <li>
-          <Link href='/login' className='nav-link'>
-            Ingresar
-          </Link>
-        </li>
-      </ul>
+      <li>
+        <Link href='/register' className='nav-link'>
+          Registro
+        </Link>
+      </li>
+      <li>
+        <Link href='/login' className='nav-link'>
+          Ingresar
+        </Link>
+      </li>
+    </ul>
+    ) : (
+    <ul className='nav-list'>
+      <li>
+        <DropdownMenu logOut={logOut}/>
+      </li>
+    </ul>
+      )}
     </nav>
   )
 }
 
 export default Navbar
+
